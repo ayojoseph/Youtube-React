62577cb22613ecf257cae0cd43d9fd56272c7bda
Unknown <ljoseph@mail.uoguelph.ca>
2018-08-17 16:43:30 -0400

    Need to fix packageJson and Version

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import _ from "lodash";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+
+const API_KEY = "AIzaSyAmfYpig2dRlhmNkQjO2Aq_gI2qHvfeYMQ";
+
+//create a new component
+//should produce some html
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch("Westbrook");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+  render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
+//take this compenent's generated html and put on the page
+
+ReactDOM.render(<App />, document.querySelector(".container"));
